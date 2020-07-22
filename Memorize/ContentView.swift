a8f8b70583430c8bb0de5e657710b768c7aa49cc
Izzy Briant <isabelbriant@gmail.com>
2020-07-22 13:54:20 +0100

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Izzy on 22/07/2020.
+//  Copyright © 2020 Isabel Briant. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
